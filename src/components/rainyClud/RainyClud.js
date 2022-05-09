21d083e5776b1b399410535a6d4ce316eb7babc0
faridhash26 <faridhash26@gmail.com>
2022-05-09 21:52:52 +0430

    adding snow comonent

---
+++ src/components/rainyClud/RainyClud.js
@@ -0,0 +1,38 @@
+import "./rainClud.css";
+import React from "react";
+
+const RainyClud = () => {
+  return (
+    <div className="container">
+      <div className="cloud"></div>
+      <div className="rain">
+        <span style={{ "--i": 11 }}></span>
+        <span style={{ "--i": 12 }}></span>
+        <span style={{ "--i": 10 }}></span>
+        <span style={{ "--i": 14 }}></span>
+        <span style={{ "--i": 18 }}></span>
+        <span style={{ "--i": 16 }}></span>
+        <span style={{ "--i": 19 }}></span>
+        <span style={{ "--i": 20 }}></span>
+        <span style={{ "--i": 12 }}></span>
+        <span style={{ "--i": 9 }}></span>
+        <span style={{ "--i": 17 }}></span>
+        <span style={{ "--i": 15 }}></span>
+        <span style={{ "--i": 11 }}></span>
+        <span style={{ "--i": 12 }}></span>
+        <span style={{ "--i": 10 }}></span>
+        <span style={{ "--i": 14 }}></span>
+        <span style={{ "--i": 18 }}></span>
+        <span style={{ "--i": 16 }}></span>
+        <span style={{ "--i": 19 }}></span>
+        <span style={{ "--i": 20 }}></span>
+        <span style={{ "--i": 12 }}></span>
+        <span style={{ "--i": 9 }}></span>
+        <span style={{ "--i": 17 }}></span>
+        <span style={{ "--i": 15 }}></span>
+      </div>
+    </div>
+  );
+};
+
+export default RainyClud;
